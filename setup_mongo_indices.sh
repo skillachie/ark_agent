27218e0b3c5ee560318c01fa6141656de024742c
Dwayne Campbell <dwaynecampbell13@gmail.com>
2014-06-14 11:35:55 -0400

    bash script to set indices

--- setup_mongo_indices.sh
+++ setup_mongo_indices.sh
@@ -1,3 +1,9 @@
 mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1,'date':1})"
 mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1})"
 mongo stocks --eval "db.load_status.ensureIndex({'symbol':1})"
+
+#Addional Indices depending on how you query your data
+#mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1,'date':1,'price_data.High':1})"
+#mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1,'date':1,'price_data.Low':1})"
+#mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1,'date':1,'price_data.Close':1})"
+#mongo stocks --eval "db.eod_data.ensureIndex({'symbol':1,'date':1,'price_data.Open':1})"
