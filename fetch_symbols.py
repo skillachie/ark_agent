5770f53a06fe19209378df3969653718daf25f94
Skillachie <dwaynecampbell13@gmail.com>
2017-06-11 19:19:52 -0400

    fetch and save symbols to db

--- fetch_symbols.py
+++ fetch_symbols.py
@@ -1,8 +1,6 @@
 import os
 import sys
 from finsymbols import symbols
-
-
 from mongo_util import MongoDBUtil
 db = MongoDBUtil()
 
@@ -10,15 +8,24 @@ def initial_symbols_load(initial=True):
 
     sp500 = symbols.get_sp500_symbols()
 
-    if db.has_symbols():
+    if db.has_symbols_loaded():
         print("Symbols already loaded")
     else:
         print("Loading initial symbols")
         db.load_symbols(sp500)
 
-def add_single_symbols(symbol):
-    #TODO if you need to collect EOD for symbol not in sp500
-    pass
+
+def add_symbols(symbols):
+
+    symbol_list = []
+    
+    for symbol in symbols:
+        sym_dict = dict()
+        sym_dict['symbol'] = symbol
+
+        symbol_list.append(sym_dict)
+
+    db.load_symbols(symbol_list)
 
 if __name__ == "__main__":
     initial_symbols_load()
--- mongo_util.py
+++ mongo_util.py
@@ -15,16 +15,25 @@ class MongoDBUtil(object):
         self.client = pymongo.MongoClient(self.config_data['mongodb']['host'],self.config_data['mongodb']['port'])
         self.db = self.client[self.config_data['mongodb']['eod_db']] 
 
-    def has_symbols(self):
+
+    def _create_indices(self):
+
+        # Symbols should be unique and only occur once
+        self.db['symbols'].create_index("symbol",unique=True)
+
+    def has_symbols_loaded(self):
         self.symbols_collection = self.db['symbols']
         if self.symbols_collection.count() < 1:
             return False
 
         return True
 
+
     def load_symbols(self,symbols):
         self.symbols_collection = self.db['symbols']
         self.symbols_collection.insert_many(symbols)
 
 
-
+if __name__ == "__main__":
+    db = MongoDBUtil()
+    db._create_indices()
